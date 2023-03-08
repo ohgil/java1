4bf9be35b975f4b2aa611e7407be7f4d2e447c4c
ohgil <ohgilhwan1957@gmail.com>
2023-03-08 12:25:49 +0900

    메세지명

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 34;
+        System.out.println("당신의 나이 : " + age);
+        //20<= age <=34
+        //청년
+        if (age >= 20 && age <= 34) {
+            System.out.println("청년입니다");
+        }
+        else {
+            System.out.println("청년이 아닙니다");
+        }
     }
 }
