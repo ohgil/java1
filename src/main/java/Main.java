44381f04e90a58ac185b4265503372eae1284366
ohgil <ohgilhwan1957@gmail.com>
2023-03-09 13:00:31 +0900

    3월 9일

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-public class Main {
+// 문제 : 100부터 1까지 출력해주세요.
+class Main {
+    public static void main(String[] args) {
+        int i = 100;
+
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+
+        for (int a = 100; a >= 1; a--) {
+            System.out.println(a);
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 34;
+        int age = 50;
         System.out.println("당신의 나이 : " + age);
-        //20<= age <=34
-        //청년
-        if (age >= 20 && age <= 34) {
-            System.out.println("청년입니다");
+
+        if (age >= 20) {
+            System.out.println("성인입니다");
         }
-        else {
-            System.out.println("청년이 아닙니다");
+        if (age < 20) {
+            System.out.println("미성년자 입니다");
         }
     }
 }
